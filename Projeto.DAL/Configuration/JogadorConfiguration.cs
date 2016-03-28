a1010b88ffe93eb96ef99735bf13a16a5158f054
taysontey <taysontey@gmail.com>
2016-03-28 14:41:59 -0300

    Entity Mapping(1)

---
+++ Projeto.DAL/Configuration/JogadorConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entity.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Projeto.DAL.Configuration
+{
+    public class JogadorConfiguration : EntityTypeConfiguration<Jogador>
+    {
+        public JogadorConfiguration()
+        {
+            ToTable("TB_JOGADOR");
+
+            HasKey(j => j.IdJogador);
+
+            Property(j => j.IdJogador)
+                .HasColumnName("IDJOGADOR")
+                .IsRequired();
+
+            Property(j => j.Nome)
+                .HasColumnName("NOME")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Property(j => j.Apelido)
+                .HasColumnName("APELIDO")
+                .HasMaxLength(25);
+
+            Property(j => j.DataNascimento)
+                .HasColumnName("DATANASCIMENTO")
+                .IsRequired();
+
+            Property(j => j.Posicao)
+                .HasColumnName("POSICAO")
+                .HasMaxLength(25)
+                .IsRequired();
+
+            Property(j => j.IdTime)
+                .HasColumnName("IDTIME")
+                .IsRequired();
+
+            #region Relacionamentos
+
+            HasRequired(j => j.Time)
+                .WithMany(t => t.Jogadores)
+                .HasForeignKey(j => j.IdTime);
+
+            #endregion
+        }
+    }
+}
---
+++ Projeto.DAL/DataSource/Conexao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entity.Entities;
+using Projeto.DAL.Configuration;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Projeto.DAL.DataSource
+{
+    public class Conexao : DbContext
+    {
+        public Conexao()
+            : base(ConfigurationManager.ConnectionStrings["projeto"].ConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new JogadorConfiguration());
+            modelBuilder.Configurations.Add(new TimeConfiguration());   
+        }
+
+        public DbSet<Jogador> Jogador { get; set; }
+        public DbSet<Time> Time { get; set; }
+    }
+}
