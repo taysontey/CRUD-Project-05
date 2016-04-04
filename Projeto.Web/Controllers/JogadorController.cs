8c325a0caf969d55910779895d5099ab32a6ef6d
taysontey <taysontey@gmail.com>
2016-04-04 17:48:20 -0300

    Cadastrar/Consultar Service (Jogador)(1)

---
+++ Projeto.Web/Controllers/JogadorController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Projeto.Web.Models;
+using Projeto.Entity.Entities;
+using Projeto.DAL.Persistence;
+
+namespace Projeto.Web.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("services/jogador")]
+    public class JogadorController : ApiController
+    {
+        [HttpPost]
+        [Route("cadastrar")]
+        public HttpResponseMessage Cadastrar(JogadorModelCadastro model)
+        {
+            try
+            {
+                Jogador j = new Jogador();
+                j.Nome = model.Nome;
+                j.Apelido = model.Apelido;
+                j.DataNascimento = model.DataNascimento;
+                j.Posicao = model.Posicao;
+                j.IdTime = model.IdTime;
+
+                JogadorDal d = new JogadorDal();
+                d.Insert(j);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Jogador cadastrado.");
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.Forbidden, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("consultar")]
+        public List<JogadorModelConsulta> Consultar()
+        {
+            try
+            {
+                List<JogadorModelConsulta> lista = new List<JogadorModelConsulta>();
+
+                JogadorDal d = new JogadorDal();
+
+                foreach(Jogador j in d.FindAll())
+                {
+                    JogadorModelConsulta model = new JogadorModelConsulta();
+                    model.IdJogador = j.IdJogador;
+                    model.Nome = j.Nome;
+                    model.Apelido = j.Apelido;
+                    model.DataNascimento = j.DataNascimento.ToString("dd/MM/yyyy");
+                    model.Posicao = j.Posicao;
+                    model.Time = j.Time.Nome;
+
+                    lista.Add(model);
+                }
+
+                return lista;
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
---
+++ Projeto.Web/Models/JogadorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto.Web.Models
+{
+    public class JogadorModelCadastro
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Posicao { get; set; }
+        public int IdTime { get; set; }
+    }
+
+    public class JogadorModelConsulta
+    {
+        public int IdJogador { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string DataNascimento { get; set; }
+        public string Posicao { get; set; }
+        public string Time { get; set; }
+    }
+}
